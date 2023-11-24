3fa8ad8410dd2a7c93670c224263e6f05a5f2d93
Brandon Banks <10030196+brandonmbanks@users.noreply.github.com>
2023-11-24 12:40:33 -0500

    animate player

--- player/player.gd
+++ player/player.gd
@@ -6,6 +6,10 @@ const FRICTION: int = 500
 
 var input_vector: Vector2 = Vector2.ZERO
 
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var animation_tree: AnimationTree = $AnimationTree
+@onready var animation_state: AnimationNodeStateMachinePlayback = animation_tree.get("parameters/playback")
+
 func _input(event: InputEvent) -> void:
 	input_vector.x = Input.get_axis("ui_left", "ui_right")
 	input_vector.y = Input.get_axis("ui_up", "ui_down")
@@ -14,8 +18,12 @@ func _physics_process(delta: float) -> void:
 	input_vector = input_vector.normalized()
 
 	if input_vector != Vector2.ZERO:
+		animation_tree.set("parameters/Idle/blend_position", input_vector)
+		animation_tree.set("parameters/Run/blend_position", input_vector)
+		animation_state.travel("Run")
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCEL * delta)
 	else:
+		animation_state.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 
 	move_and_slide()
